15966c52c80f1121529b0a9c12df5bfb6d22c68d
Torben Knerr <mail@tknerr.de>
2015-09-14 00:41:23 +0200

    add a Rakefile with sh commands for resolving deps, runnning chef, linting & testing and use that in `update-vm.sh`

--- scripts/update-vm.sh
+++ scripts/update-vm.sh
@@ -5,43 +5,66 @@ TARGET_DIR="/tmp/vagrant-cache/wget"
 SCRIPT_FILE="$(readlink -f ${BASH_SOURCE[0]})"
 REPO_ROOT="$(dirname $SCRIPT_FILE)/.."
 
-echo ""
-echo "Checking ChefDK..."
-echo ""
+big_step() {
+  echo ""
+  echo "====================================="
+  echo ">>>>>> $1"
+  echo "====================================="
+  echo ""
+}
+step() {
+  echo ""
+  echo ">>>>>> $1"
+  echo ""
+}
+
+big_step "Checking ChefDK..."
 
 if [[ $(chef -v | grep $CHEFDK_VERSION) ]]; then
   echo "ChefDK $CHEFDK_VERSION already installed"
 else
-  echo "Downloading and installing ChefDK $CHEFDK_VERSION"
+  step "Downloading and installing ChefDK $CHEFDK_VERSION"
   mkdir -p $TARGET_DIR
   wget -nc -O $TARGET_DIR/chefdk_$CHEFDK_VERSION-1_amd64.deb \
     https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_$CHEFDK_VERSION-1_amd64.deb
   sudo dpkg -i $TARGET_DIR/chefdk_$CHEFDK_VERSION-1_amd64.deb
 fi
 
-echo ""
-echo "Symlinking self as 'update-vm'..."
+
+big_step "Symlinking 'update-vm'..."
 
 sudo ln -sf $SCRIPT_FILE /usr/local/bin/update-vm
 
+
+big_step "Updating the VM via Chef..."
+
 # pull latest changes from Git?
 if [[ "$1" == "--pull" ]]; then
-  echo ""
-  echo "Pulling latest changes from git..."
-  echo ""
-
+  step "Pulling latest changes from git..."
   cd $REPO_ROOT
   git pull
 fi
 
-echo ""
-echo "Updating the VM via Chef..."
-echo ""
-
+set -e
 cd $REPO_ROOT/cookbooks/vm
 
-# install cookbook dependencies
-berks vendor ./cookbooks
+# init the shell so we have rake and rspec available
+step "init the shell"
+eval $(chef shell-init bash)
 
-# run chef-zero
-sudo chef-client --local-mode --format=doc --force-formatter --runlist=vm
+# install cookbook dependencies
+step "install cookbook dependencies"
+rake dependencies
+
+# converge the system via chef-zero
+step "trigger the chef-zero run"
+rake converge
+
+# run lint checks
+step "run codestyle checks"
+rake codestyle
+rake foodcritic
+
+# run integration tests
+step "run integration tests"
+rake integration
