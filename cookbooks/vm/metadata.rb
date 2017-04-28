bc914c440ea6e5f06f98fbb5f58ff4125877b84f
Torben Knerr <mail@tknerr.de>
2017-04-28 18:38:55 +0200

    add the license info to metadata.rb

--- cookbooks/vm/metadata.rb
+++ cookbooks/vm/metadata.rb
@@ -1,7 +1,7 @@
 name 'vm'
 maintainer 'Your Name'
 maintainer_email 'your.name@example.com'
-license 'All rights reserved'
+license 'MIT'
 description 'Installs/Configures a sample Linux Developer VM'
 long_description IO.read(File.join(File.dirname(__FILE__), '../../README.md'))
 version '0.1.0'
