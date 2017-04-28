92e49d3bb9779ad5c1da6e4aa7c1abe2cd1139c8
Torben Knerr <mail@tknerr.de>
2017-04-28 18:14:31 +0200

    fix foodcritic issue due to missing source / issues URL in metadata.rb

--- cookbooks/vm/metadata.rb
+++ cookbooks/vm/metadata.rb
@@ -5,6 +5,8 @@ license 'All rights reserved'
 description 'Installs/Configures a sample Linux Developer VM'
 long_description IO.read(File.join(File.dirname(__FILE__), '../../README.md'))
 version '0.1.0'
+issues_url 'https://github.com/zuehlke/linux-developer-vm/issues'
+source_url 'https://github.com/zuehlke/linux-developer-vm'
 
 supports 'ubuntu'
 
