5dd77b56773dc30c2d73de7e44d72d168c837e0c
Torben Knerr <mail@tknerr.de>
2017-04-30 00:55:56 +0200

    fix naming and URLs in cookbook structure

--- cookbooks/vm/metadata.rb
+++ cookbooks/vm/metadata.rb
@@ -2,11 +2,11 @@ name 'vm'
 maintainer 'Your Name'
 maintainer_email 'your.name@example.com'
 license 'MIT'
-description 'Installs/Configures a sample Linux Developer VM'
+description 'Installs/Configures a Linux Developer VM'
 long_description IO.read(File.join(File.dirname(__FILE__), '../../README.md'))
 version '0.1.0'
-issues_url 'https://github.com/zuehlke/linux-developer-vm/issues'
-source_url 'https://github.com/zuehlke/linux-developer-vm'
+issues_url 'https://github.com/Zuehlke/linux-developer-vm/issues'
+source_url 'https://github.com/Zuehlke/linux-developer-vm'
 
 supports 'ubuntu'
 
