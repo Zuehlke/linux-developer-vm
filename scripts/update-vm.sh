2881ebb6b156df2a57765082aeed998ebeb30eef
Torben Knerr <mail@tknerr.de>
2017-04-29 23:14:12 +0200

    fail early if we encounter an error in the update-vm.sh script and fix missing apt cache update

--- scripts/update-vm.sh
+++ scripts/update-vm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e -o pipefail
 
 CHEFDK_VERSION="1.3.32"
 TARGET_DIR="/tmp/vagrant-cache/wget"
@@ -40,6 +41,7 @@ check_git() {
     echo "Git already installed"
   else
     step "Installing Git"
+    sudo apt-get update
     sudo apt-get install git -y
   fi
 }
