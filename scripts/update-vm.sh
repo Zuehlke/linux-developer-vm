ef72c7fc854a72d0bafe38eeae2a05434efe1600
Torben Knerr <mail@tknerr.de>
2017-04-30 01:29:16 +0200

    speed up chefdk version check in update-vm.sh

--- scripts/update-vm.sh
+++ scripts/update-vm.sh
@@ -23,7 +23,7 @@ step() {
 
 check_chefdk() {
   big_step "Checking ChefDK..."
-  if [[ $(chef -v | grep $CHEFDK_VERSION) ]]; then
+  if [[ $(head -n1 /opt/chefdk/version-manifest.txt | grep "chefdk $CHEFDK_VERSION") ]]; then
     echo "ChefDK $CHEFDK_VERSION already installed"
   else
     step "Downloading and installing ChefDK $CHEFDK_VERSION"
