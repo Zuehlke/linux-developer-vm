de71d54d9a3a8847de7f5001b78691e6ec05cd47
Torben Knerr <mail@tknerr.de>
2017-05-02 01:42:27 +0200

    use a cookbook_file for the desktop README rather than defining it inline; also add more information

--- cookbooks/vm/recipes/base.rb
+++ cookbooks/vm/recipes/base.rb
@@ -7,37 +7,20 @@
 
 include_recipe 'apt'
 
+# base packages
 package 'vim' do
   action :install
-end
+end#
 
-# make sure it exists (e.g. when running in docker with ubuntu-14.04 image)
+# create a README on the Desktop
 directory '/home/vagrant/Desktop' do
   owner 'vagrant'
   group 'vagrant'
   mode '0755'
-  action :create
 end
-
-file '/home/vagrant/Desktop/README' do
+cookbook_file '/home/vagrant/Desktop/README' do
+  source "desktop_readme.md"
   owner 'vagrant'
   group 'vagrant'
   mode '0644'
-  action :create
-  content <<-EOF
-Developer VM README
-===================
-
-For applying the current configuration to the VM:
-
-  * simply run `update-vm`
-
-For updating to the latest configuration and applying that:
-
-  * simply run `update-vm --pull`
-
-For verifying the current configuration without applying anything:
-
-  * simply run `update-vm --verify-only`
-EOF
 end
