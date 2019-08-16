abd104fb20c9a651e9aeef61c11dc2a7c899aaa6
Torben Knerr <mail@tknerr.de>
2019-08-16 22:12:57 +0200

    fix path to README.md in metadata

--- cookbooks/vm/metadata.rb
+++ cookbooks/vm/metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Your Name'
 maintainer_email 'your.name@example.com'
 license 'MIT'
 description 'Installs/Configures a Linux Developer VM'
-long_description IO.read(File.join(File.dirname(__FILE__), '../../README.md'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 issues_url 'https://github.com/Zuehlke/linux-developer-vm/issues'
 source_url 'https://github.com/Zuehlke/linux-developer-vm'
